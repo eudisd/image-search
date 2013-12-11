5b9ffb15d51dd2d1332030d74b3a44be8f408b34
Eudis Duran <eudisd@gmail.com>
2013-12-11 00:07:14 -0500

    Clean up

--- projects/settings.py
+++ projects/settings.py
@@ -27,8 +27,6 @@ DATABASES = {
 
 TIME_ZONE = 'America/New_York'
 
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
 
 SITE_ID = 1
