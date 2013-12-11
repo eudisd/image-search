6360d309d35d4917f5c5430e9d3d8299a28c54b7
Eudis Duran <eudisd@gmail.com>
2013-12-10 23:33:16 -0500

    More clean up

--- projects/settings.py
+++ projects/settings.py
@@ -10,15 +10,9 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-#Eudis
 BASE = os.path.dirname(os.path.abspath(__name__))
 
-#BASE = '/home/prototype/repos/git/img-search/projects'
-#MY_MEDIA = '/home5/bluemedi/.local/python2.7/lib/site-packages/projects/imgsearch/static/images'
-
 TEMPLATE_BASE = BASE + "/imgsearch/templates"
-# This defines where all the file uploads go
-#MY_MEDIA = '/home/prototype/repos/git/img-search/projects/imgsearch/static/images'
 
 MY_MEDIA = BASE + '/imgsearch/static/images'
 
@@ -41,17 +35,13 @@ LANGUAGE_CODE = 'en-us'
 
 SITE_ID = 1
 
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
 USE_I18N = True
 
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale
 USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = MY_MEDIA
+#MEDIA_ROOT = MY_MEDIA
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -63,17 +53,13 @@ MEDIA_URL = ''
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
 STATIC_ROOT = ''
-#STATIC_ROOT = '/home5/bluemedi/public_html/img/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
-#Server
+
 #STATIC_URL = 'http://img.eudisduran.com/static/'
 
-# URL prefix for admin static files -- CSS, JavaScript and images.
-# Make sure to use a trailing slash.
-# Examples: "http://foo.com/static/admin/", "/static/admin/".
 ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
@@ -83,8 +69,6 @@ STATICFILES_DIRS = (
     # Don't forget to use absolute paths, not relative paths.
 )
 
-# List of finder classes that know how to find static files in
-# various locations.
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
@@ -98,7 +82,6 @@ SECRET_KEY = ''
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -112,9 +95,6 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'projects.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
     TEMPLATE_BASE
 )
 
@@ -125,18 +105,10 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
     'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
     'imgsearch'
 )
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
